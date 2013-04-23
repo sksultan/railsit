9ffd81d2c31faab3cd693080ce19705a88d6917c
Shoaib Sultan <wmcreate@gmail.com>
2013-04-23 12:40:43 -0700

    Rails skeleton checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostitSs::Application.config.secret_token = '438a4202b63830c0a33819006ce5c2dc187562180b9952bfa2764f7e6dbfe2645b7c33409b00e70e2dc7c98b311e0dcf7274bbd62cdd71ff2bbcfa41522008d6'
